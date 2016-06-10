b862c37c83a9dd0924f4b07233bf56d7cf627f19
Matt Wilkin <mwilkin22@gmail.com>
2016-06-10 14:00:24 -0700

    added console logs to track index.

--- js/scripts.js
+++ js/scripts.js
@@ -6,27 +6,25 @@ $(document).ready(function() {
     event.preventDefault();
     var input = parseInt($("input#numberInput").val());
     var result = pingPong(input);
-    console.log(result);
     $("ul#list").append("<li>" + result + "</li>");
   });
 });
 
 // Backend logic
-  var pingPong = function(input) {
-    var ping = "ping";
-    var pong = "pong";
-    var pngpng= "pingpong";
-    for (var index = 0; index < input; index++) {
-
-      if(input%15 === 0) {
-      return pngpng;
-      }
-      else if (input%5 === 0) {
-        return pong;
-      }
-      else if (input%3 === 0) {
-        return ping;
-      }
-      else return index;
-  };
+var result = "";
+var pingPong = function(input) {
+  for (var index = 1; index <= input; index++) {
+    console.log(index);
+    if (input % 15 === 0 && input % 5 === 0 && input % 3 === 0) {
+      result = "pingpong";
+    } else if (input % 5 === 0 && input % 3 === 0) {
+      result = "pong";
+    } else if (input % 3 === 0) {
+      result = "ping";
+    } else {
+      result = result;
+    }
+  }
+  console.log(result);
+  return result;
 };
